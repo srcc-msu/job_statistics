80d05cdd9b5d3163df3ca1e0a746d8bae295a56b
Pavel Shvets <shvets.pavel.srcc@gmail.com>
2016-12-21 17:02:07 +0300

    fix logic of paging and filtering

--- modules/job_table/controllers.py
+++ modules/job_table/controllers.py
@@ -1,10 +1,12 @@
 from functools import partial
 from typing import List, Optional
+import urllib
 
 from flask import Blueprint, Response, render_template, request, current_app
 import flask
 from flask_sqlalchemy import BaseQuery
 from sqlalchemy import or_
+import time
 
 from modules.job_table.helpers import get_color, calculate_job_query_stat
 from application.database import global_db
@@ -19,13 +21,10 @@ job_table_pages = Blueprint('job_table', __name__
 def construct_general_query(accounts: str, date_from: Optional[int], date_to: Optional[int]) -> BaseQuery:
 	query = global_db.session.query(Job, JobTag, JobPerformance).join(JobTag).join(JobPerformance)
 
-	if date_from is not None:
-		query = query.filter(Job.t_start > date_from)
+	query = query.filter(Job.t_start > date_from)
+	query = query.filter(Job.t_end < date_to)
 
-	if date_to is not None:
-		query = query.filter(Job.t_end < date_to)
-
-	if accounts is not None:
+	if len(accounts) > 0:
 		accounts = accounts.split(",")
 		query = query.filter(Job.account.in_(accounts))
 
@@ -51,32 +50,47 @@ def construct_filtered_query(query: BaseQuery, req_tags: List[str], opt_tags: Li
 
 	return query
 
-@job_table_pages.route("/table")
+@job_table_pages.route("/table/")
 def table_redirect():
-	return flask.redirect(flask.url_for(table, page=0))
+	query = "?" + request.query_string.decode("utf-8") if len(request.query_string) > 0 else ""
 
-@job_table_pages.route("/table/<int:page>")
+	return flask.redirect(flask.url_for("job_table.table", page=0) + query)
+
+@job_table_pages.route("/table/page/<int:page>")
 def table(page) -> Response:
 	PAGE_SIZE = 50
 
-	date_from = request.args.get("date_from")
-	date_to = request.args.get("date_to")
-	accounts = request.args.get("accounts")
+	data = request.args.to_dict()
 
-	def extract_tag(name: str):
-		tags = request.args.get(name)
+	reload = False
+
+	if "date_to" not in data or data["date_to"] == 0:
+		data["date_to"] = int(time.time())
+		reload = True
+
+	if "date_from" not in data or data["date_from"] == 0:
+		data["date_from"] = data["date_to"] - 86400 * 3
+		reload = True
+
+	if reload:
+		return flask.redirect(request.path + "?" + urllib.parse.urlencode(data))
+
+	accounts = data.get("accounts", "")
+
+	def extract_tag(data: dict, name: str):
+		tags = data.get(name)
 
 		if tags is None or len(tags) == 0:
 			return []
 		return tags.split(TAG_SEPARATOR)
 
-	req_tags = extract_tag("req_tags")
-	opt_tags = extract_tag("opt_tags")
-	no_tags = extract_tag("no_tags")
+	req_tags = extract_tag(data, "req_tags")
+	opt_tags = extract_tag(data, "opt_tags")
+	no_tags = extract_tag(data, "no_tags")
 
 #   query construction
 
-	query = construct_general_query(accounts, date_from, date_to)
+	query = construct_general_query(accounts, data["date_from"], data["date_to"])
 	query = construct_filtered_query(query, req_tags, opt_tags, no_tags)
 
 	min_len = current_app.app_config.general["aggregation_interval"] * 3
@@ -87,10 +101,12 @@ def table(page) -> Response:
 
 	show_query = query.order_by(Job.t_end.desc()).offset(page * PAGE_SIZE).limit(PAGE_SIZE)
 
+	query = "?" + request.query_string.decode("utf-8") if len(request.query_string) > 0 else ""
+
 	return render_template("job_table.html"
 		, jobs=show_query.all()
 		, query_stat=query_stat
-		, prev_page = flask.url_for("job_table.table", page = page-1) if page > 0 else None
-		, next_page = flask.url_for("job_table.table", page = page+1)
+		, prev_page = flask.url_for("job_table.table", page = page-1) + query if page > 0 else None
+		, next_page = flask.url_for("job_table.table", page = page+1) + query
 		, app_config=current_app.app_config
 		, get_color=partial(get_color, thresholds=current_app.app_config.monitoring["thresholds"]))
--- modules/job_table/static/job_table.js
+++ modules/job_table/static/job_table.js
@@ -6,62 +6,44 @@ function getParameterByName(name) {
     return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
 }
 
-function update_page(date_from, date_to, req_tags, opt_tags, no_tags)
+function load_query()
 {
-    window.location.href = location.protocol + '//' + location.host + "/job_table/table/0"
+	var date_from = $("#date_from").datepicker('getDate') / 1000;
+	var date_to = $("#date_to").datepicker('getDate') / 1000;
+
+	var accounts = $("#accounts").val();
+
+	var req_tags = $("#req_tags").tagit("assignedTags").join(";");
+	var opt_tags = $("#opt_tags").tagit("assignedTags").join(";");
+	var no_tags = $("#no_tags").tagit("assignedTags").join(";");
+
+	window.location.href = location.protocol + '//' + location.host + "/job_table/table"
         + "?date_from=" + date_from + "&date_to=" + date_to
+        + "&accounts=" + accounts
         + "&req_tags=" + req_tags
         + "&opt_tags=" + opt_tags
         + "&no_tags=" + no_tags;
 }
 
-function update_current_page()
-{
-	var date_to = $("#date_to").datepicker('getDate') / 1000;
-
-	if(date_to == 0)
-	{
-		date_to = Math.floor(Date.now() / 1000);
-	}
-
-	var date_from = $("#date_from").datepicker('getDate') / 1000;
-
-	if(date_from == 0)
-	{
-		date_from = date_to - 86400 * 3;
-	}
-
-    update_page(date_from, date_to
-    	, $("#date_to").datepicker('getDate') / 1000
-    	, $("#req_tags").tagit("assignedTags").join(";")
-        , $("#opt_tags").tagit("assignedTags").join(";")
-        , $("#no_tags").tagit("assignedTags").join(";"));
-}
-
-function set_current_page()
+function fill_form()
 {
 	var date_from = getParameterByName("date_from");
-	var date_to = getParameterByName("date_to");
-
 	if(!!date_from)
 	{
-		var prased_date_from = $.datepicker.parseDate('@', date_from * 1000);
-		$("#date_from").datepicker("setDate", prased_date_from);
+		var parsed_date_from = $.datepicker.parseDate('@', date_from * 1000);
+		$("#date_from").datepicker("setDate", parsed_date_from);
 	}
-//	else
-//		$("#date_from").datepicker("setDate", "-3");
 
+	var date_to = getParameterByName("date_to");
 	if(!!date_to)
 	{
 		var parsed_date_to = $.datepicker.parseDate('@', date_to * 1000);
 		$("#date_to").datepicker("setDate", parsed_date_to);
 	}
-//	else
-//		$("#date_to").datepicker("setDate", "today");
 
+	var accounts = getParameterByName("accounts");
 
-	var date_from = $("#date_from").datepicker('getDate') / 1000;
-	var date_to = $("#date_to").datepicker('getDate') / 1000;
+    $("#accounts").val(accounts);
 
     var filters = ["req_tags", "opt_tags", "no_tags"];
 
@@ -75,20 +57,9 @@ function set_current_page()
             $("#" + tag_holder).tagit("createTag", data[i], "", true);
         }
     }
-
-    /*var params = "?t_from=" + t_from + "&t_to=" + t_to
-    	+ "&req_tags=" + $("#req_tags").tagit("assignedTags").join(";")
-        + "&opt_tags=" + $("#opt_tags").tagit("assignedTags").join(";")
-        + "&no_tags=" + $("#no_tags").tagit("assignedTags").join(";");
-
-    var short_link = location.protocol + '//' + location.host + "/tasks" + params;
-    var long_link = location.protocol + '//' + location.host + "/tasks_ext" + params;
-
-    $("#short_table").attr("href", short_link);
-    $("#long_table").attr("href", long_link);*/
 }
 
-function InitTags()
+function init_tags()
 {
     jQuery.ajax({
         dataType: "json",
