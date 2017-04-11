5b474295bcd73870c69e12f312b3470ad5b73c03
Pavel Shvets <shvets.pavel.srcc@gmail.com>
2017-04-11 19:58:35 +0300

    add subtemplates for jd

--- modules/jd/controllers.py
+++ modules/jd/controllers.py
@@ -36,6 +36,26 @@ def jd(job_id: int, task_id: int) -> Response:
 		, app_config=current_app.app_config
 		, get_color=partial(get_color, thresholds=current_app.app_config.monitoring["thresholds"]))
 
+@jd_pages.route("/share/<string:hash>")
+def anon_jd(hash: str) -> Response:
+	task_id = 0
+	job_id = hash2id(hash)
+
+	try:
+		job = Job.get_by_id(job_id, task_id)
+	except LookupError:
+		abort(404)
+
+	tag = JobTag.query.get(job.id)
+	performance = JobPerformance.query.get(job.id)
+
+	return render_template("jd_anon.html", anon=True, hash=hash
+		, job=job.to_dict(), tags=tag.to_dict(), monitoring=performance.to_dict()
+		, derivative=current_app.app_config.monitoring["calculate_derivative"](performance.to_dict())
+		, app_config=current_app.app_config
+		, get_color=partial(get_color, thresholds=current_app.app_config.monitoring["thresholds"]))
+
+
 @jd_pages.route("/<int:job_id>/<int:task_id>/heatmap/<string:sensor>")
 @requires_auth
 def heatmap(job_id: int, task_id: int, sensor: str) -> Response:
@@ -84,21 +104,3 @@ def anon_heatmap(hash: str, sensor: str) -> Response:
 
 	return heatmap(job_id, task_id, sensor)
 
-@jd_pages.route("/share/<string:hash>")
-def anon_jd(hash: str) -> Response:
-	task_id = 0
-	job_id = hash2id(hash)
-
-	try:
-		job = Job.get_by_id(job_id, task_id)
-	except LookupError:
-		abort(404)
-
-	tag = JobTag.query.get(job.id)
-	performance = JobPerformance.query.get(job.id)
-
-	return render_template("jd.html", anon=True, hash=hash
-		, job=job.to_dict(), tags=tag.to_dict(), monitoring=performance.to_dict()
-		, derivative=current_app.app_config.monitoring["calculate_derivative"](performance.to_dict())
-		, app_config=current_app.app_config
-		, get_color=partial(get_color, thresholds=current_app.app_config.monitoring["thresholds"]))
