08ff73e931eaf2d04cc84e72b87a9f13c87efd32
Pavel Shvets <shvets.pavel.srcc@gmail.com>
2017-04-11 18:08:41 +0300

    add auth to unittests

--- test.py
+++ test.py
@@ -9,10 +9,13 @@ from core.monitoring.models import JobPerformance
 from core.tag.models import JobTag, Tag
 from core.job.helpers import id2hash, hash2id
 from modules.autotag.models import AutoTag
+import base64
 
 class TestSuit(unittest.TestCase):
 	client = None
 	setup = False
+
+	auth_headers = {}
 
 	@classmethod
 	def setUpClass(cls):
@@ -34,39 +37,51 @@ class TestSuit(unittest.TestCase):
 		TestSuit.app_context = TestSuit.app.app_context()
 		TestSuit.app_context.push()
 
+		login = TestSuit.app.config.get("LOGIN")
+		password = TestSuit.app.config.get("PASSWORD")
+
+		TestSuit.auth_headers['Authorization']\
+			= 'Basic '.encode() + base64.b64encode("{0}:{1}".format(login, password).encode())
+
 	@classmethod
 	def tearDownClass(cls):
 		pass
 
 class GeneralTestSuit(TestSuit):
 	def test_menu(self):
-		rv = TestSuit.client.get("/menu")
+		rv = TestSuit.client.get("/menu"
+			, headers = TestSuit.auth_headers)
 		assert "200" in rv.status
 		assert b"All urls" in rv.data
 
 	def test_urls(self):
-		rv = TestSuit.client.get("/urls")
+		rv = TestSuit.client.get("/urls"
+			, headers = TestSuit.auth_headers)
 		print(rv.status, rv.data)
 
 		assert "200" in rv.status
 		assert b"/urls" in rv.data
 
 	def test_preset(self):
-		rv = TestSuit.client.get("/job_stat/preset")
+		rv = TestSuit.client.get("/job_stat/preset"
+			, headers = TestSuit.auth_headers)
 		print(rv.status, rv.data)
 
 		assert "200" in rv.status
 		assert b"General statistics" in rv.data
 
 	def test_constructor(self):
-		rv = TestSuit.client.get("/job_stat/constructor")
+		rv = TestSuit.client.get("/job_stat/constructor"
+			, headers = TestSuit.auth_headers)
 		print(rv.status, rv.data)
 
 		assert "200" in rv.status
 		assert b"General settings" in rv.data
 
 	def test_table(self):
-		rv = TestSuit.client.get("/job_table/table", follow_redirects=True)
+		rv = TestSuit.client.get("/job_table/table" # bug with redirect and headers?
+			, follow_redirects=True
+			, headers = TestSuit.auth_headers)
 		print(rv.status, rv.data)
 
 		assert "200" in rv.status
@@ -83,13 +98,15 @@ class TestJob(TestSuit):
 		add(global_db, cls.test_job)
 
 	def test_short(self):
-		rv = TestSuit.client.get("/jd/1")
+		rv = TestSuit.client.get("/jd/1"
+			, headers = TestSuit.auth_headers)
 		print(rv.status, rv.data)
 
 		assert "302" in rv.status
 
 	def test_direct(self):
-		rv = TestSuit.client.get("/jd/1/1")
+		rv = TestSuit.client.get("/jd/1/1"
+			, headers = TestSuit.auth_headers)
 		print(rv.status, rv.data)
 
 		assert "200" in rv.status
@@ -99,7 +116,8 @@ class TestJob(TestSuit):
 		rv = TestSuit.client.post("/api/job/"
 			, data={"format" : "slurm_db"
 				, "stage": "BEFORE"
-				, "data": "4;test_create_job_slurm_db;test;./;gputest;2;3;4;node1-001-01;1;1;5;6;7"})
+				, "data": "4;test_create_job_slurm_db;test;./;gputest;2;3;4;node1-001-01;1;1;5;6;7"}
+			, headers = TestSuit.auth_headers)
 
 		print(rv.status, rv.data)
 
@@ -115,7 +133,8 @@ class TestJob(TestSuit):
 		rv = TestSuit.client.post("/api/job/"
 			, data={"format" : "sacct"
 				, "stage": "BEFORE"
-				, "data" : "5476|compute|serg|2016-01-14T17:21:23|2016-01-14T17:21:23|2016-01-14T17:21:26|1-00:00:00|COMPLETED|4294900803|2|20|n[48021-48022,51000-51010]"})
+				, "data" : "5476|compute|serg|2016-01-14T17:21:23|2016-01-14T17:21:23|2016-01-14T17:21:26|1-00:00:00|COMPLETED|4294900803|2|20|n[48021-48022,51000-51010]"}
+			, headers=TestSuit.auth_headers)
 
 		print(rv.status, rv.data)
 
@@ -131,12 +150,14 @@ class TestJob(TestSuit):
 		_ = TestSuit.client.post("/api/job/"
 			, data={"format" : "sacct"
 				, "stage": "BEFORE"
-				, "data" : "54761|compute|tttttttt|2016-01-14T17:21:23|2016-01-14T17:21:23|2016-01-14T17:21:26|1-00:00:00|COMPLETED|4294900803|2|20|n[48021-48022,51000-51010]"})
+				, "data" : "54761|compute|tttttttt|2016-01-14T17:21:23|2016-01-14T17:21:23|2016-01-14T17:21:26|1-00:00:00|COMPLETED|4294900803|2|20|n[48021-48022,51000-51010]"}
+			, headers = TestSuit.auth_headers)
 
 		rv = TestSuit.client.post("/api/job/"
 			, data={"format" : "sacct"
 				, "stage": "AFTER"
-				, "data" : "54761|compute|tttttttt|2016-01-14T17:21:23|2016-01-14T17:21:23|2016-01-14T17:21:26|1-00:00:00|COMPLETED|4294900803|2|20|n[48021-48022,51000-51010]"})
+				, "data" : "54761|compute|tttttttt|2016-01-14T17:21:23|2016-01-14T17:21:23|2016-01-14T17:21:26|1-00:00:00|COMPLETED|4294900803|2|20|n[48021-48022,51000-51010]"}
+			, headers = TestSuit.auth_headers)
 
 		print(rv.status, rv.data)
 
@@ -152,7 +173,8 @@ class TestJob(TestSuit):
 		rv = TestSuit.client.post("/api/job/"
 			, data={"format" : "sacct"
 				, "stage": "ONLY_MISSING"
-				, "data" : "54763|compute|tttt|2016-01-14T17:21:23|2016-01-14T17:21:23|2016-01-14T17:21:26|1-00:00:00|COMPLETED|4294900803|2|20|n[48021-48022,51000-51010]"})
+				, "data" : "54763|compute|tttt|2016-01-14T17:21:23|2016-01-14T17:21:23|2016-01-14T17:21:26|1-00:00:00|COMPLETED|4294900803|2|20|n[48021-48022,51000-51010]"}
+			, headers = TestSuit.auth_headers)
 
 		print(rv.status, rv.data)
 
@@ -162,7 +184,8 @@ class TestJob(TestSuit):
 		rv = TestSuit.client.post("/api/job/"
 			, data={"format" : "sacct"
 				, "stage": "ONLY_MISSING"
-				, "data" : "54763|compute|tttt|2016-01-14T17:21:23|2016-01-14T17:21:23|2016-01-14T17:21:26|1-00:00:00|COMPLETED|4294900803|2|20|n[48021-48022,51000-51010]"})
+				, "data" : "54763|compute|tttt|2016-01-14T17:21:23|2016-01-14T17:21:23|2016-01-14T17:21:26|1-00:00:00|COMPLETED|4294900803|2|20|n[48021-48022,51000-51010]"}
+			, headers = TestSuit.auth_headers)
 
 		print(rv.status, rv.data)
 
@@ -178,7 +201,8 @@ class TestJob(TestSuit):
 		rv = TestSuit.client.post("/api/job/"
 			, data={"format" : "slurm_plugin"
 				, "stage": "BEFORE"
-				, "data": "JobId=1142390 Name=impi UserId=test_create_job_slurm_plugin_2(655) GroupId=test_create_job_slurm_plugin(655) Priority=152 Account=test_create_job_slurm_plugin QOS=normal JobState=COMPLETED Reason=None Dependency=(null) Requeue=0 Restarts=0 BatchFlag=1 ExitCode=0:0 DerivedExitCode=0:0 RunTime=00:59:54 TimeLimit=3-00:00:00 TimeMin=N/A SubmitTime=2015-10-30T13:45:57 EligibleTime=2015-10-30T13:45:57 StartTime=2015-10-30T13:45:57 EndTime=2015-11-02T13:45:57 PreemptTime=None SuspendTime=None SecsPreSuspend=0 Partition=regular6 AllocNode:Sid=access2:16866 ReqNodeList=(null) ExcNodeList=(null) NodeList=node4-132-[04-07],node4-134-[07-09],node4-136-[04-07],node4-138-[25-28],node4-142-[22-26],node4-144-[04-07],node4-146-[13-17],node4-148-[04-08],node4-149-[31-32],node4-150-[01-02,04-08] BatchHost=node4-132-04 NumNodes=43 NumCPUs=516 CPUs/Task=1 ReqS:C:T=*:*:*   Nodes=node4-132-[04-07],node4-134-[07-09],node4-136-[04-07],node4-138-[25-28],node4-142-[22-26],node4-144-[04-07],node4-146-[13-17],node4-148-[04-08],node4-149-[31-32],node4-150-[01-02,04-08] CPU_IDs=0-11 Mem=0 MinCPUsNode=1 MinMemoryNode=0 MinTmpDiskNode=0 Features=(null) Gres=(null) Reservation=(null) Shared=0 Contiguous=0 Licenses=(null) Network=(null) Command=/opt/mpi/wrappers/impi ./cdynak WorkDir=/mnt/msu/users/ztsv/bm_l5_r6"})
+				, "data": "JobId=1142390 Name=impi UserId=test_create_job_slurm_plugin_2(655) GroupId=test_create_job_slurm_plugin(655) Priority=152 Account=test_create_job_slurm_plugin QOS=normal JobState=COMPLETED Reason=None Dependency=(null) Requeue=0 Restarts=0 BatchFlag=1 ExitCode=0:0 DerivedExitCode=0:0 RunTime=00:59:54 TimeLimit=3-00:00:00 TimeMin=N/A SubmitTime=2015-10-30T13:45:57 EligibleTime=2015-10-30T13:45:57 StartTime=2015-10-30T13:45:57 EndTime=2015-11-02T13:45:57 PreemptTime=None SuspendTime=None SecsPreSuspend=0 Partition=regular6 AllocNode:Sid=access2:16866 ReqNodeList=(null) ExcNodeList=(null) NodeList=node4-132-[04-07],node4-134-[07-09],node4-136-[04-07],node4-138-[25-28],node4-142-[22-26],node4-144-[04-07],node4-146-[13-17],node4-148-[04-08],node4-149-[31-32],node4-150-[01-02,04-08] BatchHost=node4-132-04 NumNodes=43 NumCPUs=516 CPUs/Task=1 ReqS:C:T=*:*:*   Nodes=node4-132-[04-07],node4-134-[07-09],node4-136-[04-07],node4-138-[25-28],node4-142-[22-26],node4-144-[04-07],node4-146-[13-17],node4-148-[04-08],node4-149-[31-32],node4-150-[01-02,04-08] CPU_IDs=0-11 Mem=0 MinCPUsNode=1 MinMemoryNode=0 MinTmpDiskNode=0 Features=(null) Gres=(null) Reservation=(null) Shared=0 Contiguous=0 Licenses=(null) Network=(null) Command=/opt/mpi/wrappers/impi ./cdynak WorkDir=/mnt/msu/users/ztsv/bm_l5_r6"}
+			, headers = TestSuit.auth_headers)
 
 		print(rv.status, rv.data)
 
@@ -194,7 +218,8 @@ class TestJob(TestSuit):
 		rv = TestSuit.client.post("/api/job/"
 			, data={"format" : "slurm_plugin"
 				, "stage": "BEFORE"
-				, "data": "JobId=98865 JobName=3cont.sh UserId=test_create_job_slurm_plugin_15(10035) GroupId=users(10000) Priority=100000000 Nice=0 Account=ivanov QOS=normal JobState=RUNNING Reason=None Dependency=(null) Requeue=0 Restarts=0 BatchFlag=1 Reboot=0 ExitCode=0:0 DerivedExitCode=0:0 RunTime=6-01:17:37 TimeLimit=6-22:40:00 TimeMin=N/A SubmitTime=2016-10-26T15:12:09 EligibleTime=2016-10-26T15:12:09 StartTime=2016-10-26T15:12:12 EndTime=2016-11-02T13:52:12 PreemptTime=None SuspendTime=None SecsPreSuspend=0 Partition=compute AllocNode:Sid=access-02:29092 ReqNodeList=(null) ExcNodeList=(null) NodeList=n50216 BatchHost=n50216 NumNodes=1 NumCPUs=14 CPUs/Task=1 ReqB:S:C:T=0:0:*:* TRES=cpu=14,node=1 Socks/Node=* NtasksPerN:B:S:C=0:0:*:* CoreSpec=*   Nodes=n50216 CPU_IDs=0-13 Mem=0 MinCPUsNode=1 MinMemoryNode=0 MinTmpDiskNode=0 Features=(null) Gres=(null) Reservation=(null) Shared=0 Contiguous=0 Licenses=(null) Network=(null) Command=/mnt/scratch/users/ivanov/something/pull_spec_collapsed_2048/3cont.sh WorkDir=/mnt/scratch/users/ivanov/something/pull_spec_collapsed_2048 StdErr=/mnt/scratch/users/ivanov/something/pull_spec_collapsed_2048/slurm-98865.out StdIn=/dev/null StdOut=/mnt/scratch/users/ivanov/something/pull_spec_collapsed_2048/slurm-98865.out Power= SICP=0"})
+				, "data": "JobId=98865 JobName=3cont.sh UserId=test_create_job_slurm_plugin_15(10035) GroupId=users(10000) Priority=100000000 Nice=0 Account=ivanov QOS=normal JobState=RUNNING Reason=None Dependency=(null) Requeue=0 Restarts=0 BatchFlag=1 Reboot=0 ExitCode=0:0 DerivedExitCode=0:0 RunTime=6-01:17:37 TimeLimit=6-22:40:00 TimeMin=N/A SubmitTime=2016-10-26T15:12:09 EligibleTime=2016-10-26T15:12:09 StartTime=2016-10-26T15:12:12 EndTime=2016-11-02T13:52:12 PreemptTime=None SuspendTime=None SecsPreSuspend=0 Partition=compute AllocNode:Sid=access-02:29092 ReqNodeList=(null) ExcNodeList=(null) NodeList=n50216 BatchHost=n50216 NumNodes=1 NumCPUs=14 CPUs/Task=1 ReqB:S:C:T=0:0:*:* TRES=cpu=14,node=1 Socks/Node=* NtasksPerN:B:S:C=0:0:*:* CoreSpec=*   Nodes=n50216 CPU_IDs=0-13 Mem=0 MinCPUsNode=1 MinMemoryNode=0 MinTmpDiskNode=0 Features=(null) Gres=(null) Reservation=(null) Shared=0 Contiguous=0 Licenses=(null) Network=(null) Command=/mnt/scratch/users/ivanov/something/pull_spec_collapsed_2048/3cont.sh WorkDir=/mnt/scratch/users/ivanov/something/pull_spec_collapsed_2048 StdErr=/mnt/scratch/users/ivanov/something/pull_spec_collapsed_2048/slurm-98865.out StdIn=/dev/null StdOut=/mnt/scratch/users/ivanov/something/pull_spec_collapsed_2048/slurm-98865.out Power= SICP=0"}
+			, headers = TestSuit.auth_headers)
 
 		print(rv.status, rv.data)
 
@@ -274,7 +299,9 @@ class TestJobTag(TestSuit):
 
 	def test_tag_complex(self):
 		def test_register_tag():
-			rv = TestSuit.client.post("/api/tag/test", data={"description": "test"})
+			rv = TestSuit.client.post("/api/tag/test"
+				, data={"description": "test"}
+				, headers = TestSuit.auth_headers)
 			print(rv.status, rv.data)
 
 			assert "200" in rv.status
@@ -287,14 +314,17 @@ class TestJobTag(TestSuit):
 			assert b"test" in rv.data
 
 		def test_add_job_tag():
-			rv = TestSuit.client.post("/api/job/{0}/tag/test".format(TestJobTag.test_job.id), data={"action" : "add"})
+			rv = TestSuit.client.post("/api/job/{0}/tag/test".format(TestJobTag.test_job.id)
+				, data={"action" : "add"}
+				, headers=TestSuit.auth_headers)
 			print(rv.status, rv.data)
 
 			assert "200" in rv.status
 			assert b"true" in rv.data
 
 		def test_delete_job_tag():
-			rv = TestSuit.client.post("/api/job/{0}/tag/test".format(TestJobTag.test_job.id), data={"action" : "delete"})
+			rv = TestSuit.client.post("/api/job/{0}/tag/test".format(TestJobTag.test_job.id), data={"action" : "delete"}
+				, headers=TestSuit.auth_headers)
 			print(rv.status, rv.data)
 
 			assert "200" in rv.status
@@ -342,14 +372,17 @@ class TestAutoTag(TestSuit):
 		global_db.session.commit()
 
 	def test_list(self):
-		rv = TestSuit.client.get("/autotag/list")
+		rv = TestSuit.client.get("/autotag/list"
+			, headers = TestSuit.auth_headers)
 		print(rv.status, rv.data)
 
 		assert "200" in rv.status
 		assert b"t_end" in rv.data
 
 	def test_create(self):
-		rv = TestSuit.client.post("/api/autotag/", data={"label": "test2", "condition": "true"})
+		rv = TestSuit.client.post("/api/autotag/"
+			, data={"label": "test2", "condition": "true"}
+			, headers = TestSuit.auth_headers)
 		print(rv.status, rv.data)
 
 		assert AutoTag.query.get(eval(rv.data)["id"]) != None
@@ -358,18 +391,22 @@ class TestAutoTag(TestSuit):
 		assert b"test2" in rv.data
 
 	def test_delete(self):
-		rv = TestSuit.client.post("/api/autotag/", data={"label": "test2", "condition": "true"})
+		rv = TestSuit.client.post("/api/autotag/"
+			, data={"label": "test2", "condition": "true"}
+			, headers = TestSuit.auth_headers)
 		print(rv.status, rv.data)
 
 		id = eval(rv.data)["id"]
 
-		rv = TestSuit.client.post("/api/autotag/" + str(id), data={"action": "delete"})
+		rv = TestSuit.client.post("/api/autotag/" + str(id)
+			, data={"action": "delete"}, headers = TestSuit.auth_headers)
 
 		assert "200" in rv.status
 		assert AutoTag.query.get(id) == None
 
 	def test_job_update(self):
-		rv = TestSuit.client.post("/api/job/1/autotag")
+		rv = TestSuit.client.post("/api/job/1/autotag"
+			, headers = TestSuit.auth_headers)
 		print(rv.status, rv.data)
 
 		assert "200" in rv.status
